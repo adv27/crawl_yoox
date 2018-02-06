a7a0496f4d41cee8fa35a6644849c8337052f6cd
anhvd <anhvdse04605@fpt.edu.vn>
2018-02-06 13:12:22 +0700

    new commit

--- get_sale_items.py
+++ get_sale_items.py
@@ -1,36 +1,29 @@
 __AUTHOR__ = 'anhvd'
 
 import re
+from multiprocessing.dummy import Pool as ThreadPool
 from re import sub
+
 import requests
-from selenium import webdriver
 from bs4 import BeautifulSoup
-import io
 
-REQUIRED_SALE_PERCENT = float(25)
+REQUIRED_SALE_PERCENT = float(40)
 ORIGINAL_URL = 'https://www.yoox.com'
-URL = r'https://www.yoox.com/hk/women/shoponline?dept=wprm_1712we&gender=D&page=1&size=3,4&season=X'
-PATH_TO_CHROMEDRIVER = r'/home/anhvd/Downloads/chromedriver'
-
-#driver = webdriver.Chrome(PATH_TO_CHROMEDRIVER)
+URL = r'https://www.yoox.com/hk/women/shoponline/leather%20jackets_c?dept=women&gender=D&page=2&attributes=%7b%27ctgr%27%3a%5b%27gbbttpll%27%5d%7d&season=X'
 
 
-def save_html(html_code, file_name):
-    with io.open(file_name,'w',encoding='utf-8') as file:
-        file.write(str(html_code))
+def save_to_html(items: []):
+    for _list in items:
+        for item in _list:
+            append_html_file(item)
+
 
 def append_html_file(item):
-    # make the img appear and change the link
-    # print('Len: ', len(list_items))
-    # for item in list_items:
-    # print('Item: ', str(item))
     anchor = item.find('a')
-    # print(str(anchor))
     anchor['href'] = ORIGINAL_URL + anchor['href']
     img = anchor.find('img')
     img['src'] = img['data-original']
     with open('result' + '.html', 'a', encoding='utf-8') as file:
-        # for item in list_items:
         file.write(str(item))
 
 
@@ -38,12 +31,9 @@ def get_html_code(url: str):
     return requests.get(url=url).text
 
 
-def get_items(url: str = None, index: int = None ):
+def get_items(url: str = None):
     if url is not None:
-        # driver.get(url=url)
-        # html_code = driver.page_source
         html_code = get_html_code(url=url)
-        # save_html(html_code,str(index) + '.html')
         soup = BeautifulSoup(html_code, 'html.parser')
         items = soup.find_all('div', {'id': re.compile('^item_')})
         return items
@@ -51,28 +41,27 @@ def get_items(url: str = None, index: int = None ):
 
 
 def items_filter(items: list, required_sale_percent: float):
-    # approved_items = list()
+    approved_items = []
     for item in items:
         old_price = item.find('span', {'class': 'oldprice text-linethrough text-light'})
         if old_price is not None:
             old_price = old_price.text
             old_price = float(sub(r'[^\d,]', '', old_price).replace(',', '.'))
-            # old_price = [float(s.replace(',', '.')) for s in old_price.split() if s.replace(',', '').replace('.','').isdigit()][0]
             new_price = item.find('span', {'class': 'newprice font-bold'}).text
             new_price = float(sub(r'[^\d,]', '', new_price).replace(',', '.'))
-            # new_price = [float(s.replace(',', '.')) for s in new_price.split() if s.replace(',', '').replace('.','').isdigit()][0]
             sale_percent = (new_price / old_price) * 100;
             if sale_percent <= required_sale_percent:
-                # approved_items += item
-                append_html_file(item=item)
-                print('Old: ', old_price, ' New: ', new_price, ' Sale Percent', sale_percent)
-                # print(len(approved_items))
-                # return approved_items
+                # append_html_file(item=item)
+                approved_items.append(item)
+                brand = item.find('div', {'class': 'brand font-bold'}).text.strip()
+                category = item.find('div', {'class': 'microcategory font-sans'}).text.strip()
+                print('{} {} Old: {} New: {} After Sale Percent: {}'.
+                      format(brand, category, old_price, new_price, sale_percent))
+    return approved_items
 
 
 def get_total_pages(url: str = None) -> int:
     '''
-
     :param url: the url of list items
     :return: number of pages
     '''
@@ -80,32 +69,31 @@ def get_total_pages(url: str = None) -> int:
         html_code = get_html_code(url=url)
         soup = BeautifulSoup(html_code, 'html.parser')
         ul = soup.find('ul', {'class': 'pagination list-inline pull-right text-center js-pagination'})
-        if ul.find('li', {'class': 'next-page'}) is not None:
-            return int(ul.find_all('li')[-2].text)
-        else:
-            return int(ul.find_all('li')[-1].text)
+        lis = ul.find_all('li')
+        return int(lis[-2].text) \
+            if ul.find('li', {'class': 'next-page'}) is not None \
+            else int(lis[-1].text)
     return 0
 
 
-def main():
+def process(url):
+    print(url)
+    items = get_items(url)
+    if items is not None:
+        return items_filter(items=items, required_sale_percent=REQUIRED_SALE_PERCENT)
+    return None
+
+
+def main() -> int:
     total_pages = get_total_pages(url=URL)
     print("Total Pages: ", total_pages)
     if total_pages is not 0:
-        # get all items from all pages
-        for i in range(1, total_pages+1):
-            url = re.sub('page=\d+', 'page=' + str(i), URL)
-            # url = url.replace('#/','?')
-            print(url)
-            items = get_items(url=url,index=i)
-            approved_items_len = 0
-            if items is not None:
-                # approved_items = items_filter(items=items, required_sale_percent=REQUIRED_SALE_PERCENT)
-                items_filter(items=items, required_sale_percent=REQUIRED_SALE_PERCENT)
-                # approved_items_len = len(approved_items)
-                # print('Approved items len: ', approved_items_len)
-                # append_html_file(list_items=approved_items)
-            print('Page: ', i, ' Items: ', len(items))
-
+        urls = [re.sub('page=\d+', 'page=' + str(i), URL) for i in range(1, total_pages + 1)]
+        pool = ThreadPool(20)
+        result = pool.map(process, urls)
+        pool.close()
+        pool.join()
+        save_to_html(result)
     return 0
 
 
